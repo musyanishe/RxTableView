40b66fa9ae3362e8dd803fb195fe79dc98008f22
Evgeniia Shevtsova <EAShevtsova@sportmasterlab.net>
2022-12-06 10:13:59 +0300

    Initial Commit

---
+++ RxTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxTableView
+//
+//  Created by Evgeniya Shevtsova on 06.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
